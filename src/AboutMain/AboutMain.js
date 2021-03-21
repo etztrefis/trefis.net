892bb597049a72131ae0d91060ac43e5d9de88db
etztrefis <etztrefishello@gmail.com>
2021-03-21 15:50:23 +0700

    :art: dankspin.gif & nerd_face.png pics, added dankspin to /about component

--- src/AboutMain/AboutMain.js
+++ src/AboutMain/AboutMain.js
@@ -3,7 +3,9 @@ import "./AboutMain.css";
 function Main() {
 	return (
 		<div>
-			<p className="main-text-here">Okayeg</p>
+			<p className="main-text-here">
+				<img src="./img/dankspin.gif" alt="Nerd Face" style={{height:"64px", width:"64px"}}/>
+			</p>
 		</div>
 	);
 }
