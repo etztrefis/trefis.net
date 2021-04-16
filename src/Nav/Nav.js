b1fa0152576ab21e0c0df139b5790f121fa8f03b
etztrefis <etztrefishello@gmail.com>
2021-04-16 17:36:49 +0700

    :art: redesign for navba

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,66 +1,60 @@
-import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
-
 import "./Nav.css";
 
 function SiteNav() {
 	return (
-		<Navbar className="backgroungcolor-nav" variant="dark" expand="lg">
-			<Navbar.Brand href="/" style={{ paddingRight: "10px" }}>
-				<img
-					alt=""
-					src="./img/pphop.gif"
-					width="50px"
-					height="50px"
-					className="d-inline-block align-top"
-				/>{" "}
-			</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="mr-auto" style={{ fontSize: "16px" }}>
-					<NavDropdown
-						title="peepoChat"
-						id="collasible-nav-dropdown"
-						style={{ paddingRight: "10px" }}
-					>
-						<NavDropdown.Item
-							href="https://peepochat.github.io/"
+		<div>
+			<Navbar className="backgroungcolor-nav" variant="dark" expand="lg">
+				<Navbar.Brand href="/">
+					<img
+						alt=""
+						src="./img/pphop.gif"
+						width="50px"
+						height="50px"
+					/>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+				<Navbar.Collapse id="responsive-navbar-nav">
+					<Nav className="mr-auto">
+						<NavDropdown
+							title="peepoChat"
+							id="collasible-nav-dropdown"
+							style={{ paddingRight: "10px" }} >
+							<NavDropdown.Item
+								href="https://chat.peepo.club/"
+								target="_blank"
+							>
+								Project
+							</NavDropdown.Item>
+							<NavDropdown.Item
+								href="https://github.com/peepoclub/peepoChat"
+								target="_blank"
+							>
+								Source Code
+							</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Link
+							href="https://eaterybot.trefis.net/"
 							target="_blank"
-						>
-							Project
-					</NavDropdown.Item>
-						<NavDropdown.Item
-							href="https://github.com/peepoChat/peepochat.github.io"
-							target="_blank"
-						>
-							Source Code
-					</NavDropdown.Item>
-					</NavDropdown>
-					<Nav.Link
-						href="https://eaterybot.trefis.net/"
-						target="_blank"
-						style={{ paddingRight: "20px" }}
-						rel="noopener"
-					>
-						EateryBot
-				</Nav.Link>
-					<NavDropdown
-						title="FeelsOkayegBot"
-						id="collasible-nav-dropdown"
-						style={{ paddingRight: "10px" }}
-					>
-						<NavDropdown.Item
+							style={{ paddingRight: "20px" }}
+							rel="noopener" >
+							EateryBot
+						</Nav.Link>
+						<Nav.Link
 							href="https://github.com/etztrefis/feelsokayegbot"
 							target="_blank"
-						>
-							Source Code
-					</NavDropdown.Item>
-					</NavDropdown>
-					<Nav.Link href="/about">About</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-		</Navbar>
+							style={{ paddingRight: "20px" }}
+							rel="noopener" >
+							FeelsOkayegBot
+						</Nav.Link>
+						<Nav.Link href="/about" className="nav-item">About</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<div className="tab-highlighter"></div>
+		</div>
 	);
 }
 
